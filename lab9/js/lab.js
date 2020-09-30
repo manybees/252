aac3c7a6219e97405d8ac21dba4840ec8e2ace70
manybees <63517497+manybees@users.noreply.github.com>
2020-09-30 14:09:10 -0700

    Create lab.js

---
+++ lab9/js/lab.js
@@ -0,0 +1,56 @@
+//Kalyn Daum
+//Lets GO!
+
+
+// Find my div#output
+
+function main() {
+    var outputEL= document.getElementById("output");
+    };
+
+// assign new element?
+
+//var new1El = document.createElement("p");
+
+function main() {
+    var new1El = document.getElementById("p");
+    new1El.innerHTML = "Something New OwO?";
+	};
+
+// assign new, new, element?
+
+//var new2El = document.createElement("s");
+
+function main2() {
+    var new2El = document.getElementById("s");
+    new2El.innerHTML = "Something New OwO 2?";
+	};
+
+// Lecture Example
+var buttonElement = document.createElement("div")
+
+	buttonElement.id = "button"
+	buttonElement.innerHTML = "Press me!";
+
+document.body.appendChild(buttonElement);
+
+//mine??
+var buttonElement = document.createElement("div")
+
+	buttonElement.id = "button"
+	buttonElement.innerHTML = "Press me!";
+
+document.body.appendChild(buttonElement);
+
+
+//css element cHanGe
+function change() {
+	var change1 = document.getElementById("h2");
+	document.getElementById("h2").style.color = "red";
+};
+
+function change2() {
+	var change2 = document.getElementById("h2");
+	document.getElementById("h2").style.color = "navy";
+};
+
