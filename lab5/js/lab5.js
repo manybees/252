f5d5f90b4df407eee23e19a4ae65595d3d133ef2
manybees <63517497+manybees@users.noreply.github.com>
2020-09-30 14:08:27 -0700

    Create lab5.js

---
+++ lab5/js/lab5.js
@@ -0,0 +1,25 @@
+/*
+*
+*
+* Author: Kalyn, kdaum@csumb.edu
+* created: 9/15
+*/
+//Define Variables
+var carMake = "Honda";
+var carModel = "Fit";
+var carColor = "Red";
+var carYear = 2004;
+var currentYear = 2020;
+
+//calculation
+var age = currentYear - carYear;
+
+//output
+
+document.write("Make: " + carMake + "<br>" ) ;
+	
+document.write(" Model: " + carModel + "<br>");
+
+document.write(" Color: " + carColor + "<br>");
+
+document.write(" Age: " + age + " years " + "<br>" );
