2c15a614c169d3440843d2d52dea78653b6a692f
manybees <63517497+manybees@users.noreply.github.com>
2020-11-18 14:17:10 -0800

    Create index.js

---
+++ lab20/js/index.js
@@ -0,0 +1,17 @@
+//Kalyn Daum
+//Hangman the Game
+
+//1.choose initial word
+//2.begin the game loop
+//	3.make the spaces for the letters entered in step 1
+//	4.have player input a letter
+//	5.check for win condition
+
+//If condition is not met, add one to a counter (up to 5 rep. bodyparts)
+
+//set up game [hangman] {create Players ,[Hangsman(player1)],[Player], Have [Hangsman] input a word}
+// create gameboard with proper spaces [board]
+// game loop [Hangman] begins
+// if word is guessed display [Victory!]
+//end loop
+//else: loop continues
